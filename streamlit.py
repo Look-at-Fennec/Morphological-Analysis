4b2fbf1ee1773542b8b481beaf4cc3bd94e77ec4
Nakanishi Kazuki <50660421+Look-at-Fennec@users.noreply.github.com>
2021-10-22 14:42:37 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -68,7 +68,7 @@ if (s1!="")&(s2!=""):
             corpus[i, word_to_index[w]] = 1
     
     if st.checkbox("show vector"):
-        st.write(pd.DataFrame(corpus[]).T)
+        st.write(pd.DataFrame(corpus).T)
         st.write(np.dot(corpus[0],corpus[1]))
 
     
